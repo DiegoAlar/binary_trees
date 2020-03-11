a08330b619a867982f743042fe0becbd8d526113
DiegoAlar <diegoaav@hotmail.com>
2020-03-11 17:24:28 -0500

    fixing betty of task 13

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -6,18 +6,16 @@
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-    int l = 0, r = 0;
+	int l = 0, r = 0;
 
-    if (!tree)
-        return (0);
-    if (tree->left)
-        l = binary_tree_nodes(tree->left);
-    if (tree->right)
-        r = binary_tree_nodes(tree->right);
-    if (tree->left || tree->right)
-        return(l + r + 1);
-    else
-        return (0);
-    
-    
+	if (!tree)
+		return (0);
+	if (tree->left)
+		l = binary_tree_nodes(tree->left);
+	if (tree->right)
+		r = binary_tree_nodes(tree->right);
+	if (tree->left || tree->right)
+		return (l + r + 1);
+	else
+		return (0);
 }
