bf7fec3639107d35e66467b85983a5013fd3e495
DiegoAlar <diegoaav@hotmail.com>
2020-03-11 17:20:50 -0500

    adding exeercise 13

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_nodes - counts the nodes with at least 1 child in a binary tree
+ * @tree: pointer to the root node of the tree to count the number of nodes
+ * Return: number of nodes
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+    int l = 0, r = 0;
+
+    if (!tree)
+        return (0);
+    if (tree->left)
+        l = binary_tree_nodes(tree->left);
+    if (tree->right)
+        r = binary_tree_nodes(tree->right);
+    if (tree->left || tree->right)
+        return(l + r + 1);
+    else
+        return (0);
+    
+    
+}
